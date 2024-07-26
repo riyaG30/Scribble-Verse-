1813f496dbc487ec3b321afe5c76ccb774b60ec0
riyaG30 <riya.rg3010@gmail.com>
2024-07-26 14:38:31 +0530

    chanbed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "public", // Set the root directory to 'public'
     base: "",
     build: {
-        outDir: "../dist", // Output directory
         rollupOptions: {
-            input: "public/index.html", // Specify the input HTML file
+            input: {
+                main: resolve(__dirname, "public/index.html"), // Ensure this path is correct
+            },
         },
     },
 });
